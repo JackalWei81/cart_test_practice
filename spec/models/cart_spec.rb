961ad5c6bfdef10322f8398d7fe482509a23c3e0
jackalwei <jackal8125@gmail.com>
2016-07-07 11:44:26 +0800

    test "can add item to cart"

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Cart features" do
+    it "can add item to Cart" do
+      cart = Cart.new
+      expect(cart.empty?).to be true
+      cart.add_item(1)
+      expect(cart.empty?).to be false
+    end
+  end
 end
