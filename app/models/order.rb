8b001a7cf95d43df18fb9f0302beff5dbe98d362
jackalwei <jackal8125@gmail.com>
2016-07-14 12:38:09 +0800

    add serial_generator

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,9 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
 
   validates :address, presence: true
+
+
+  def serial_generator(id)
+    "OD#{id.to_s.rjust(10,'0')}"
+  end
 end
